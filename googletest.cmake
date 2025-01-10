06892be6cfb941982e3cec00b327669efa5b193a
Nikolay Grin <Nikolai_Grin@Mail.Ru>
2025-01-10 18:42:40 +0100

    HW_1: Complete the task
    Also:
    - Add CI pipeline for pull requests
    - Add new GCC and the latest CMake to CI
    - Add auto-downloading Boost during the build via CMake
    - Add auto-finding the package for CI release
    
    Signed-off-by: Nikolay Grin <Nikolai_Grin@Mail.Ru>

---
+++ googletest.cmake
@@ -0,0 +1,11 @@
+find_package(GTest 1.17.0)
+if(NOT GTest_FOUND)
+  include(FetchContent)
+  FetchContent_Declare(
+    GTest
+    GIT_REPOSITORY https://github.com/google/googletest.git
+    GIT_TAG v1.17.0
+    GIT_SHALLOW ON
+    EXCLUDE_FROM_ALL)
+  FetchContent_MakeAvailable(GTest)
+endif()
