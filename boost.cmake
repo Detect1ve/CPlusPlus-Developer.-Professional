b608e451325848b419ba7c03f64146dfe23d2ee6
Nikolay Grin <Nikolai_Grin@Mail.Ru>
2025-01-10 18:42:40 +0100

    HW_1: Complete the task
    Also:
    - Add CI pipeline for pull requests
    - Add new GCC and the latest CMake to CI
    - Add auto-downloading Boost during the build via CMake
    - Add auto-finding the package for CI release
    
    Signed-off-by: Nikolay Grin <Nikolai_Grin@Mail.Ru>

---
+++ boost.cmake
@@ -0,0 +1,12 @@
+find_package(Boost 1.88.0 CONFIG COMPONENTS unit_test_framework)
+if (NOT Boost_FOUND)
+  include(FetchContent)
+  set(BOOST_INCLUDE_LIBRARIES test)
+  FetchContent_Declare(
+    Boost
+    URL https://github.com/boostorg/boost/releases/download/boost-1.88.0/boost-1.88.0-cmake.tar.xz
+    URL_MD5 3edffaacd2cfe63c240ef1b99497c74f
+    DOWNLOAD_EXTRACT_TIMESTAMP TRUE
+    EXCLUDE_FROM_ALL)
+  FetchContent_MakeAvailable(Boost)
+endif()
