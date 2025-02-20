22da72da82f3eebfb9de67b92ab8da4c5e86bfc4
Nikolay Grin <Nikolai_Grin@Mail.Ru>
2025-02-20 23:16:01 +0100

    HW_3: Complete the task
    Signed-off-by: Nikolay Grin <Nikolai_Grin@Mail.Ru>

---
+++ HW_3/include/my_list.hpp
@@ -0,0 +1,138 @@
+#ifndef MY_LIST_HPP
+#define MY_LIST_HPP
+
+#include <gsl/gsl>
+
+template <typename T, typename Alloc = std::allocator<T>>
+class MyList
+{
+    class Node;
+public:
+    MyList() = default;
+    ~MyList()
+    {
+        using NodeAllocator =
+            typename std::allocator_traits<Alloc>::template rebind_alloc<Node>;
+        NodeAllocator nodeAllocator;
+
+        Node* current_ = head_;
+        while (current_)
+        {
+            Node* next_ = current_->getNext();
+
+            current_->~Node();
+            nodeAllocator.deallocate(current_, 1);
+            current_ = next_;
+        }
+    }
+    MyList(const MyList&) = delete;
+    MyList& operator=(const MyList&) = delete;
+    MyList(MyList&&) = delete;
+    MyList& operator=(MyList&&) = delete;
+
+    class iterator
+    {
+    public:
+        explicit iterator(Node *const ptr) : current_(ptr) {}
+
+        iterator& operator++()
+        {
+            if (current_)
+            {
+                current_ = current_->getNext();
+            }
+
+            return *this;
+        }
+
+        T operator*() const
+        {
+            return current_->getValue();
+        }
+
+        bool operator!=(const iterator& other) const
+        {
+            return current_ != other.current_;
+        }
+
+    private:
+        Node* current_;
+    };
+
+    iterator begin()
+    {
+        return iterator(head_);
+    }
+
+    iterator end()
+    {
+        return iterator(nullptr);
+    }
+
+    [[nodiscard]] bool empty() const
+    {
+        return head_ == nullptr;
+    }
+
+    [[nodiscard]] std::size_t size() const
+    {
+        return size_;
+    }
+
+    void push_back(const T& val)
+    {
+        using NodeAllocator =
+            typename std::allocator_traits<Alloc>::template rebind_alloc<Node>;
+        NodeAllocator nodeAllocator;
+
+        // NOLINTNEXTLINE(cppcoreguidelines-owning-memory)
+        auto const newNode = static_cast<gsl::owner<Node*>>(nodeAllocator.allocate(1));
+
+        new (newNode) Node(val);
+
+        if (head_ == nullptr)
+        {
+            head_ = newNode;
+            tail_ = newNode;
+        }
+        else
+        {
+            tail_->setNext(newNode);
+            tail_ = newNode;
+        }
+
+        size_++;
+    }
+
+private:
+    class Node
+    {
+    public:
+        explicit Node(T const val): next_(nullptr), val_(val) {}
+
+        [[nodiscard]] T getValue() const
+        {
+            return val_;
+        }
+
+        [[nodiscard]] Node* getNext() const
+        {
+            return next_;
+        }
+
+        void setNext(Node *const newNext)
+        {
+            next_ = newNext;
+        }
+
+    private:
+        Node *next_;
+        T val_;
+    };
+
+    Node *head_ = nullptr;
+    Node *tail_ = nullptr;
+    std::size_t size_ = 0;
+};
+
+#endif // MY_LIST_HPP
---
+++ HW_3/src/main.cpp
@@ -0,0 +1,85 @@
+#include <cstdlib> // EXIT_FAILURE
+#include <exception> // std::exception
+#include <functional> // std::less
+#include <map>
+#include <utility> // std::pair
+
+#include <custom_print.hpp>
+#include <my_allocator.hpp>
+#include <my_list.hpp>
+
+constexpr unsigned char CONTAINER_SIZE = 10;
+
+namespace
+{
+    constexpr unsigned factorial(const unsigned number)
+    {
+        unsigned ret = 1;
+
+        for (unsigned i = 2; i <= number; i++)
+        {
+            ret *= i;
+        }
+
+        return ret;
+    }
+} // namespace
+
+int main()
+{
+    try
+    {
+        std::map<int, int> map_with_standard_alloc;
+
+        for (unsigned i = 0; i < CONTAINER_SIZE; i++)
+        {
+            map_with_standard_alloc.emplace(i, factorial(i));
+        }
+
+        std::map<int, int, std::less<>,
+            MyAllocator<std::pair<const int, int>, CONTAINER_SIZE>> map_with_custom_alloc;
+
+        for (unsigned i = 0; i < CONTAINER_SIZE; i++)
+        {
+            map_with_custom_alloc.emplace(i, factorial(i));
+        }
+
+        for (const auto& elem : map_with_custom_alloc)
+        {
+            cp::println("{} {}", elem.first, elem.second);
+        }
+
+        MyList<unsigned> my_list_with_standard_alloc;
+
+        for (unsigned i = 0; i < CONTAINER_SIZE; i++)
+        {
+            my_list_with_standard_alloc.push_back(i);
+        }
+
+        MyList<unsigned, MyAllocator<int, CONTAINER_SIZE>> my_list_with_custom_alloc;
+
+        for (unsigned i = 0; i < CONTAINER_SIZE; i++)
+        {
+            my_list_with_custom_alloc.push_back(i);
+        }
+
+        for (const auto elem : my_list_with_custom_alloc)
+        {
+            cp::println("{}", elem);
+        }
+
+        return 0;
+    }
+    catch (const std::exception& e)
+    {
+        cp::safe_error(e.what());
+
+        return EXIT_FAILURE;
+    }
+    catch (...)
+    {
+        cp::safe_error(nullptr);
+
+        return EXIT_FAILURE;
+    }
+}
