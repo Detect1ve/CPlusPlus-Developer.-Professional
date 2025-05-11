20a7f2d511f3d13529d575a15a8dc191356dbb35
Nikolay Grin <Nikolai_Grin@Mail.Ru>
2025-05-11 15:53:58 +0200

    HW_7: Complete the task
    Signed-off-by: Nikolay Grin <Nikolai_Grin@Mail.Ru>

---
+++ HW_7/include/taskmanager.hpp
@@ -0,0 +1,48 @@
+#ifndef TASKMANAGER_HPP
+#define TASKMANAGER_HPP
+
+#include <chrono>
+#include <iostream>
+#if __GNUC__ < 14
+#include <vector>
+#endif
+
+namespace bulk
+{
+    class taskmanager
+    {
+    public:
+        taskmanager(
+            const int        max_task_count,
+            std::string_view name)
+            :
+            max_static_task_count(max_task_count),
+            task_manager_name(name) { };
+
+        taskmanager(const taskmanager&) = delete;
+        taskmanager& operator=(const taskmanager&) = delete;
+        taskmanager(taskmanager&&) = delete;
+        taskmanager& operator=(taskmanager&&) = delete;
+
+        [[nodiscard]] int run(std::istream& input);
+        [[nodiscard]] int run();
+
+        ~taskmanager() noexcept;
+
+    private:
+        int add_task(std::string_view task);
+        [[nodiscard]] bool is_dynamic_block_active() const noexcept;
+        int process_tasks(std::vector<std::string>& block_task);
+
+        int dynamic_block_nesting_level = 0;
+        int task_count = 0;
+        int max_static_task_count;
+        std::chrono::system_clock::time_point dynamic_block_timestamp;
+        std::chrono::system_clock::time_point static_block_timestamp;
+        std::string task_manager_name;
+        std::vector<std::string> dynamic_block_task;
+        std::vector<std::string> static_block_task;
+    };
+} // namespace bulk
+
+#endif /* TASKMANAGER_HPP */
