781fb74fa70563f6cd9c916172a4752b1b8ee82b
Nikolay Grin <Nikolai_Grin@Mail.Ru>
2025-05-11 15:53:58 +0200

    HW_7: Complete the task
    Signed-off-by: Nikolay Grin <Nikolai_Grin@Mail.Ru>

---
+++ HW_7/lib/taskmanager.cpp
@@ -0,0 +1,313 @@
+#include <chrono> // std::chrono::system_clock
+#if !defined(_WIN32) && !defined(_MSC_VER)
+#include <csignal>
+#endif
+#include <cstdio> // stderr
+#include <exception> // std::exception
+#include <filesystem>
+#include <fstream>
+#include <iostream>
+#include <string> // std::string
+#include <string_view> // std::string_view
+#include <type_traits> // std::remove_reference_t
+#include <vector> // std::vector
+#include <version> // IWYU pragma: keep
+
+#ifdef __cpp_lib_ranges_to_container
+#include <ranges>
+#else
+#include <cstddef> // std::size_t
+#endif
+
+#include <custom_print.hpp>
+#include <taskmanager.hpp>
+
+namespace bulk::io
+{
+    class composite_stream
+    {
+    public:
+        composite_stream(
+            std::ostream& stream1,
+            std::ostream& stream2)
+            :
+            s1(stream1),
+            s2(stream2) {}
+
+        composite_stream(const composite_stream&) = delete;
+        composite_stream& operator=(const composite_stream&) = delete;
+        composite_stream(composite_stream&&) = delete;
+        composite_stream& operator=(composite_stream&&) = delete;
+        ~composite_stream() = default;
+
+
+        template<typename T>
+        composite_stream& operator<<(const T& value)
+        {
+            if constexpr (std::is_array_v<std::remove_reference_t<T>>)
+            {
+                s1 << static_cast<const char*>(value);
+                s2 << static_cast<const char*>(value);
+            }
+            else
+            {
+                s1 << value;
+                s2 << value;
+            }
+
+            return *this;
+        }
+
+        composite_stream& operator<<(std::ostream& (*manip)(std::ostream&))
+        {
+            s1 << manip;
+            s2 << manip;
+
+            return *this;
+        }
+
+    private:
+        std::ostream& s1;
+        std::ostream& s2;
+    };
+} // namespace bulk::io
+
+namespace
+{
+    [[nodiscard]] bulk::taskmanager*& get_task_manager_instance() noexcept
+    {
+        static bulk::taskmanager* instance = nullptr; // NOLINT(misc-const-correctness)
+
+        return instance;
+    }
+
+#if !defined(_WIN32) && !defined(_MSC_VER)
+    void signal_handler(const int /*signal*/)
+    {
+        if (auto* instance = get_task_manager_instance(); instance)
+        {
+            instance->stop();
+        }
+    }
+#endif
+} // namespace
+
+namespace bulk
+{
+    [[nodiscard]] bool taskmanager::is_dynamic_block_active() const noexcept
+    {
+        return dynamic_block_nesting_level > 0;
+    }
+
+    int taskmanager::add_task(const std::string_view task)
+    {
+        static constexpr std::string_view CLOSE_BRACKET = "}";
+        static constexpr std::string_view OPEN_BRACKET = "{";
+        int ret = 0;
+
+        if (task == OPEN_BRACKET)
+        {
+            if (!taskmanager::is_dynamic_block_active())
+            {
+                dynamic_block_timestamp = std::chrono::system_clock::now();
+                ret = process_tasks(static_block_task);
+            }
+
+            dynamic_block_nesting_level++;
+
+            return ret;
+        }
+
+        if (task == CLOSE_BRACKET)
+        {
+            dynamic_block_nesting_level--;
+            if (!taskmanager::is_dynamic_block_active())
+            {
+                ret = process_tasks(dynamic_block_task);
+            }
+            // Special case, when user try to enter "}" string as command
+            if (dynamic_block_nesting_level < 0)
+            {
+                dynamic_block_nesting_level = 0;
+            }
+            else
+            {
+                return ret;
+            }
+        }
+
+        if (taskmanager::is_dynamic_block_active())
+        {
+            dynamic_block_task.emplace_back(task);
+        }
+        else
+        {
+            if (static_block_timestamp == std::chrono::system_clock::time_point{})
+            {
+                static_block_timestamp = std::chrono::system_clock::now();
+            }
+
+            static_block_task.emplace_back(task);
+            if (++task_count % max_static_task_count == 0)
+            {
+                ret = process_tasks(static_block_task);
+            }
+        }
+
+        return ret;
+    }
+
+    int taskmanager::process_tasks(std::vector<std::string>& block_task)
+    {
+        constexpr std::string_view DELIMITER = ", ";
+        int ret = 0;
+
+        if (block_task.empty()) [[unlikely]]
+        {
+            return ret;
+        }
+
+        std::chrono::system_clock::time_point *timestamp =
+            (task_count != 0 ? &static_block_timestamp : &dynamic_block_timestamp);
+        // NOLINTNEXTLINE(misc-include-cleaner)
+        auto timestamp_seconds = std::chrono::floor<std::chrono::seconds>(
+            timestamp->time_since_epoch()).count();
+
+        const std::filesystem::path log_path =
+            std::filesystem::path(task_manager_name).filename().string() +
+            std::to_string(timestamp_seconds) + ".log";
+
+        std::ofstream file(log_path);
+
+        if (!file.is_open())
+        {
+            cp::println(stderr, "Failed to open file");
+            ret = -1;
+
+            return ret;
+        }
+
+        bulk::io::composite_stream output(std::cout, file);
+
+        output << std::filesystem::path(task_manager_name).filename().string() << ": ";
+
+#ifdef __cpp_lib_ranges_to_container
+        const std::string result = block_task | std::views::join_with(DELIMITER)
+            | std::ranges::to<std::string>();
+#else
+        std::string result;
+
+        for (std::size_t i = 0; i < block_task.size(); i++)
+        {
+            result += block_task[i];
+            if (i + 1 < block_task.size())
+            {
+                result += DELIMITER;
+            }
+        }
+#endif
+        output << result << '\n';
+
+        block_task.clear();
+
+        *timestamp = std::chrono::system_clock::time_point{};
+        task_count = 0;
+
+        return ret;
+    }
+
+    int taskmanager::run()
+    {
+        return run(std::cin);
+    }
+
+    int taskmanager::run(std::istream& input)
+    {
+        int ret = 0;
+        std::string line;
+
+        try
+        {
+            while (!stop_flag)
+            {
+                if (std::getline(input, line))
+                {
+                    if (this->add_task(line) != 0)
+                    {
+                        cp::println(stderr, "Error processing command: {}", line);
+                    }
+                }
+                else if (input.eof() || input.fail())
+                {
+                    break;
+                }
+            }
+        }
+        catch (const std::exception& e)
+        {
+            cp::println(stderr, "Fatal error: {}", e.what());
+            ret = -1;
+        }
+
+        return ret;
+    }
+
+    void taskmanager::stop() noexcept
+    {
+        stop_flag = true;
+    }
+
+    void taskmanager::setup_signal_handling()
+    {
+        get_task_manager_instance() = this;
+#if !defined(_WIN32) && !defined(_MSC_VER)
+// NOLINTBEGIN(misc-include-cleaner)
+        struct sigaction sigaction_info {};
+
+        sigaction_info.sa_handler = signal_handler;
+        sigemptyset(&sigaction_info.sa_mask);
+        sigaction_info.sa_flags = 0;
+        if (sigaction(SIGHUP, &sigaction_info, nullptr) == -1)
+        {
+            cp::println(stderr, "Failed to register SIGHUP handler, but continue anyway");
+        }
+
+        if (sigaction(SIGINT, &sigaction_info, nullptr) == -1)
+        {
+            cp::println(stderr, "Failed to register SIGINT handler, but continue anyway");
+        }
+
+        if (sigaction(SIGQUIT, &sigaction_info, nullptr) == -1)
+// NOLINTEND(misc-include-cleaner)
+        {
+            cp::println(stderr,
+                "Failed to register SIGQUIT handler, but continue anyway");
+        }
+
+        if (sigaction(SIGTERM, &sigaction_info, nullptr) == -1)
+        {
+            cp::println(stderr,
+                "Failed to register SIGTERM handler, but continue anyway");
+        }
+#endif
+    }
+
+    taskmanager::~taskmanager() noexcept
+    {
+        try
+        {
+            if (!taskmanager::is_dynamic_block_active())
+            {
+                process_tasks(static_block_task);
+            }
+        }
+        catch (const std::exception& e)
+        {
+            cp::safe_error(e.what());
+        }
+        catch (...)
+        {
+            cp::safe_error("error in destructor");
+        }
+    }
+} // namespace bulk
---
+++ HW_7/src/main.cpp
@@ -0,0 +1,79 @@
+#if __GNUC__ < 14
+#include <charconv>
+#endif
+#include <cstddef> // std::size_t
+#include <cstdio> // stderr
+#include <cstdlib> // EXIT_FAILURE
+#include <exception> // std::exception
+#include <ranges>
+#include <span> // std::span
+#include <string_view> // std::string_view
+#include <system_error> // std::errc
+
+#include <custom_print.hpp>
+#include <taskmanager.hpp>
+
+int main(
+    int const          argc,
+    const char **const argv)
+{
+    auto const args = std::span(argv, static_cast<std::size_t>(argc))
+        | std::views::transform([](char const *const arg) noexcept
+        {
+            return std::string_view(arg);
+        });
+    constexpr unsigned char BASE = 10;
+    int command_number = 0;
+    int ret = 0;
+
+    try
+    {
+        if (args.size() < 2)
+        {
+            cp::println(stderr, "Usage: {} <positive_number>", args[0]);
+            ret = -1;
+
+            return ret;
+        }
+        // NOLINTNEXTLINE(cppcoreguidelines-pro-bounds-pointer-arithmetic)
+        if (std::from_chars(args[1].data(), args[1].data() + args[1].size(),
+            command_number, BASE).ec != std::errc{})
+        {
+            cp::println(stderr, "Invalid number format");
+            ret = -2;
+
+            return ret;
+        }
+
+        if (command_number <= 0)
+        {
+            cp::println(stderr, "Block size must be a positive number");
+            ret = -3;
+
+            return ret;
+        }
+
+        bulk::taskmanager my_task_manager(command_number, args[0]);
+
+        my_task_manager.setup_signal_handling();
+        ret = my_task_manager.run();
+        if (ret != 0)
+        {
+            cp::println(stderr, "run return {}", ret);
+        }
+    }
+    catch (const std::exception& e)
+    {
+        cp::safe_error(e.what());
+
+        ret = EXIT_FAILURE;
+    }
+    catch (...)
+    {
+        cp::safe_error(nullptr);
+
+        ret = EXIT_FAILURE;
+    }
+
+    return ret;
+}
