5b09653657ce5f0d898a219a79c7cf93dea3d8d8
Nikolay Grin <Nikolai_Grin@Mail.Ru>
2025-05-12 18:23:04 +0200

    HW_8: Complete the task
    Signed-off-by: Nikolay Grin <Nikolai_Grin@Mail.Ru>

---
+++ HW_8/src/main.cpp
@@ -0,0 +1,29 @@
+#include <iostream>
+#include <utility>
+
+#include <bayan.hpp>
+
+int main(
+    const int          argc,
+    const char **const argv)
+{
+    auto [status, options] = option_process({argv, static_cast<std::size_t>(argc)});
+
+    if (status == ProcessStatus::HELP_REQUESTED)
+    {
+        return std::to_underlying(ProcessStatus::SUCCESS);
+    }
+
+    if (status != ProcessStatus::SUCCESS)
+    {
+        return std::to_underlying(status);
+    }
+
+    status = process_files(options);
+    if (status != ProcessStatus::SUCCESS)
+    {
+        std::cerr << "process_files return " << +std::to_underlying(status) << '\n';
+    }
+
+    return std::to_underlying(status);
+}
