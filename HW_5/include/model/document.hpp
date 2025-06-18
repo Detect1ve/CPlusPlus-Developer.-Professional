601a0bb5d7820171d7e9988fa8a9fc9d30aee98e
Nikolay Grin <Nikolai_Grin@Mail.Ru>
2025-06-18 20:55:58 +0200

    HW_5: Complete the task
    Signed-off-by: Nikolay Grin <Nikolai_Grin@Mail.Ru>

---
+++ HW_5/include/model/document.hpp
@@ -0,0 +1,44 @@
+#ifndef MODEL_DOCUMENT_HPP
+#define MODEL_DOCUMENT_HPP
+
+#if __cplusplus <= 201703L
+#include <string>
+#endif
+#include <vector>
+
+#include <model/primitive.hpp>
+
+namespace editor::model
+{
+    class Document
+    {
+    public:
+        explicit Document(std::string name);
+        ~Document() = default;
+
+        Document(const Document&) = delete;
+        Document& operator=(const Document&) = delete;
+        Document(Document&&) = delete;
+        Document& operator=(Document&&) = delete;
+
+        void addPrimitive(std::unique_ptr<Primitive> primitive);
+        void removePrimitive(std::size_t index);
+        [[nodiscard]] const Primitive* getPrimitive(std::size_t index) const;
+        [[nodiscard]] std::size_t getPrimitiveCount() const;
+
+        [[nodiscard]] const std::string& getName() const;
+        void setName(const std::string& name);
+
+        [[nodiscard]] static bool saveToFile(
+            const Document&    document,
+            const std::string& filename);
+
+        static std::unique_ptr<Document> loadFromFile(const std::string& filename);
+
+    private:
+        std::string name_;
+        std::vector<std::unique_ptr<Primitive>> primitives_;
+    };
+} // namespace editor::model
+
+#endif /* MODEL_DOCUMENT_HPP */
---
+++ HW_5/include/model/primitives/line.hpp
@@ -0,0 +1,61 @@
+#ifndef MODEL_PRIMITIVES_LINE_HPP
+#define MODEL_PRIMITIVES_LINE_HPP
+
+#include <model/primitive.hpp>
+
+namespace editor::model
+{
+    class Line : public Primitive
+    {
+    public:
+        Line(
+            struct Point start,
+            struct Point end,
+            int thickness)
+            :
+            x1_(start.x),
+            y1_(start.y),
+            x2_(end.x),
+            y2_(end.y),
+            thickness_(thickness) {}
+
+        [[nodiscard]] std::string getType() const override
+        {
+            return "Line";
+        }
+
+        [[nodiscard]] int getX1() const
+        {
+            return x1_;
+        }
+
+        [[nodiscard]] int getY1() const
+        {
+            return y1_;
+        }
+
+        [[nodiscard]] int getX2() const
+        {
+            return x2_;
+        }
+
+        [[nodiscard]] int getY2() const
+        {
+            return y2_;
+        }
+
+        [[nodiscard]] int getThickness() const
+        {
+            return thickness_;
+        }
+
+    private:
+        int x1_;
+        int y1_;
+        int x2_;
+        int y2_;
+        int thickness_;
+    };
+} // namespace editor::model
+
+#endif /* MODEL_PRIMITIVES_LINE_HPP */
