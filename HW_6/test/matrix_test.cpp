f0fdedc869e9c2289dc3fa97ad5e91abf67a38ed
Nikolay Grin <Nikolai_Grin@Mail.Ru>
2025-06-19 23:00:11 +0200

    HW_6: Complete the task
    Signed-off-by: Nikolay Grin <Nikolai_Grin@Mail.Ru>

---
+++ HW_6/test/matrix_test.cpp
@@ -0,0 +1,74 @@
+#include <gtest/gtest.h>
+
+#include <capture.hpp>
+#include <matrix.hpp>
+
+enum : uint16_t
+{
+    TEST_VALUE_1 = 314,
+    TEST_VALUE_2 = 217,
+};
+
+TEST(HW6, Matrix)
+{
+    // infinite matrix int filled with values ​​-1
+    Matrix<int, -1> matrix;
+    std::string capturedStdout;
+
+    ASSERT_EQ(0, matrix.size());
+
+    auto a = matrix[0][0];
+
+    ASSERT_EQ(-1, a);
+    ASSERT_EQ(0, matrix.size());
+    matrix[100][100] = TEST_VALUE_1;
+    ASSERT_EQ(TEST_VALUE_1, matrix[100][100]);
+    ASSERT_EQ(1, matrix.size());
+
+    // one row will be printed
+    // 100100314
+    StdoutCapture::Begin();
+    for (auto c: matrix)
+    {
+        int x = 0;
+        int y = 0;
+        int v = 0;
+
+        std::tie(x, y, v) = c;
+
+        std::cout << x << y << v << std::endl;
+
+        ASSERT_EQ(100, x);
+        ASSERT_EQ(100, y);
+        ASSERT_EQ(TEST_VALUE_1, v);
+    }
+
+    capturedStdout = StdoutCapture::End();
+
+    ASSERT_EQ("100100314\n", capturedStdout);
+
+    ((matrix[100][100] = TEST_VALUE_1) = 0) = TEST_VALUE_2;
+
+    ASSERT_EQ(TEST_VALUE_2, matrix[100][100]);
+    ASSERT_EQ(1, matrix.size());
+
+    StdoutCapture::Begin();
+    for (auto c: matrix)
+    {
+        int x = 0;
+        int y = 0;
+        int v = 0;
+
+        std::tie(x, y, v) = c;
+
+        std::cout << x << y << v << std::endl;
+
+        ASSERT_EQ(100, x);
+        ASSERT_EQ(100, y);
+        ASSERT_EQ(TEST_VALUE_2, v);
+    }
+
+    capturedStdout = StdoutCapture::End();
+    ASSERT_EQ("100100217\n", capturedStdout);
+
+}
