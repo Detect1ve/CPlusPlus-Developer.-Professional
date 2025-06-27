037d78672f4339408c888ab4d6f8821b82b974e2
Nikolay Grin <Nikolai_Grin@Mail.Ru>
2025-06-27 22:14:16 +0200

    HW_12: Complete the task
    Signed-off-by: Nikolay Grin <Nikolai_Grin@Mail.Ru>

--- HW_12/reducer.cpp
+++ HW_12/reducer.cpp
@@ -1,15 +1,31 @@
 #include <iostream>
 #include <string>
 
+#include <sstream>
 
 int main(int argc, char ** argv)
 {
     size_t count = 0;
     std::string line;
+    double sum = 0.0;
+
     while (std::getline(std::cin, line))
     {
-        count += 1;
+        std::istringstream iss(line);
+        std::string key;
+        double price;
+        long value_count;
+
+        if (iss >> key >> price >> value_count)
+        {
+            sum += price * value_count;
+            count += value_count;
+        }
     }
-    std::cout << count << std::endl;
+
+    double mean = (count > 0) ? (sum / count) : 0.0;
+
+    std::cout << "Mean price: " << mean << std::endl;
+
     return 0;
 }
