80fdf179f85fbc68afd4a7e13668904cb5530218
Nikolay Grin <Nikolai_Grin@Mail.Ru>
2025-06-28 13:25:55 +0200

    HW_8: Fixes after review
    - Add shortcut alias for help
    - Output the options during the unrecognised option error
    
    Signed-off-by: Nikolay Grin <Nikolai_Grin@Mail.Ru>

--- HW_8/lib/bayan.cpp
+++ HW_8/lib/bayan.cpp
@@ -458,7 +458,7 @@ std::pair<ProcessStatus, Options> option_process(std::span<const char *const> ar
     try
     {
         cmdline_options.add_options()
-            ("help", "produce help message");
+            ("help,h", "produce help message");
         mandatory_options.add_options()
             ("block_size", boost::program_options::value<decltype(options.block_size)>
                 (&options.block_size)->required()->notifier(negative_check),
@@ -514,6 +514,7 @@ std::pair<ProcessStatus, Options> option_process(std::span<const char *const> ar
     catch (const boost::program_options::error& e)
     {
         std::cerr << e.what() << '\n';
+        std::cerr << cmdline_options;
         ret = ProcessStatus::OPTION_ERROR;
     }
 
