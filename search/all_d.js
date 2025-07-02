98733f7827a8066c845363f903b253ed9838cd5b
Detect1ve <Detect1ve@users.noreply.github.com>
2025-07-02 12:55:06 +0000

    Deploy Doxygen documentation for 351c1d30176cb873ebaf1ed3d7dfb38a842e6700

---
+++ search/all_d.js
@@ -0,0 +1,4 @@
+var searchData=
+[
+  ['verification_0',['Verification',['../index.html#autotoc_md2',1,'']]]
+];
---
+++ search/pages_6.js
@@ -0,0 +1,5 @@
+var searchData=
+[
+  ['implement_20the_20function_20of_20printing_20a_20conditional_20ip_20address_0',['&lt;b&gt;Implement the function of printing a conditional IP address.&lt;/b&gt;',['../index.html#autotoc_md1',1,'']]],
+  ['ip_20address_1',['&lt;b&gt;Implement the function of printing a conditional IP address.&lt;/b&gt;',['../index.html#autotoc_md1',1,'']]]
+];
