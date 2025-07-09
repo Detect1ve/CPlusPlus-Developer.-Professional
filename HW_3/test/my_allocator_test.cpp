ff0d81e14872a725849cce1bc4cb658292de34b4
Nikolay Grin <Nikolai_Grin@Mail.Ru>
2025-07-09 13:22:17 +0200

    HW_3: Adapt for Windows
    Signed-off-by: Nikolay Grin <Nikolai_Grin@Mail.Ru>

--- HW_3/test/my_allocator_test.cpp
+++ HW_3/test/my_allocator_test.cpp
@@ -11,17 +11,8 @@ TEST(HW_3, my_allocator)
 {
     std::vector<int, MyAllocator<int, CONTAINER_SIZE>> vector_with_custom_alloc;
 
-    try
+    ASSERT_THROW(
     {
         vector_with_custom_alloc.reserve(CONTAINER_SIZE + 1);
-        FAIL() << "std::bad_alloc";
-    }
-    catch(std::bad_alloc const & err)
-    {
-        ASSERT_EQ(err.what(), std::string("std::bad_alloc"));
-    }
-    catch(...)
-    {
-        FAIL() << "Expected std::bad_alloc";
-    }
+    }, std::bad_alloc);
 }
